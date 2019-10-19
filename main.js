bdbb8ba9eca7edb698df0af1ef9b61296fa0f887
cloudhub2all <55729309+cloudhub2all@users.noreply.github.com>
2019-10-19 13:11:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello NodeApp5555!! \n");
+  response.end("Hello NodeApp0000!! \n");
 });
 
 // listen on localhost:7800
