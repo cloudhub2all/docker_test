5cf86a96b6abc3fdf8e97151580dd826d0e2ba54
cloudhub2all <55729309+cloudhub2all@users.noreply.github.com>
2019-10-19 12:29:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello NodeApp881133!! \n");
+  response.end("Hello NodeApp!! \n");
 });
 
-// listen on localhost:8000
-server.listen(8000);
-console.log("Server listening on port 8000 :  http://127.0.0.1:8000/");
+// listen on localhost:7800
+server.listen(7800);
+console.log("Server listening on port 7800 :  http://127.0.0.1:7800/");
